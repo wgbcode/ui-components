5c114c084b8f4c22631f9606e5a33d7bdf6be81f
wuguangbin <2567199685@qq.com>
2022-06-20 10:21:27 +0800

    使自定义滚动条随内容滚动而滚动

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./scroll.scss";
 
-const Scroll: React.FC = () => {
-  let scrollbarWidth = () => {
+interface Props extends React.HTMLAttributes<HTMLElement> {}
+
+const Scroll: React.FC<Props> = (props) => {
+  const [barHeight, setBarHeight] = useState(0);
+  const [barTop, setBarTop] = useState(0);
+  const scrollbarWidth = () => {
     const div = document.createElement("div");
     div.style.position = "absolute";
     div.style.top = div.style.left = "-9999px"; // 把 div 放到屏幕之外，防止影响用户
@@ -13,9 +17,30 @@ const Scroll: React.FC = () => {
     document.body.removeChild(div);
     return width;
   };
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const div = containerRef.current;
+    const scrollHeight = div!.scrollHeight;
+    const viewHeight = div!.getBoundingClientRect().height;
+    setBarHeight((viewHeight * viewHeight) / scrollHeight);
+  });
+
+  const onScroll = () => {
+    const div = containerRef.current;
+    const scrollHeight = div!.scrollHeight;
+    const viewHeight = div!.getBoundingClientRect().height;
+    const scrollTopHeight = div!.scrollTop;
+    setBarTop((scrollTopHeight * viewHeight) / scrollHeight);
+  };
+
   return (
     <div className="fui-scroll">
-      <div className="fui-scroll-item" style={{ right: -scrollbarWidth() }}>
+      <div
+        className="fui-scroll-item"
+        style={{ right: -scrollbarWidth() }}
+        onScroll={onScroll}
+        ref={containerRef}
+      >
         <p>1</p>
         <p>2</p>
         <p>3</p>
@@ -37,6 +62,11 @@ const Scroll: React.FC = () => {
         <p>19</p>
         <p>20</p>
       </div>
+      <div className="fui-scroll-track"></div>
+      <div
+        className="fui-scroll-bar"
+        style={{ height: barHeight, top: barTop }}
+      ></div>
     </div>
   );
 };
