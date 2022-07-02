3a45ab342f34d15e7aaf0ba3c05bac69b93d25b2
wuguangbin <2567199685@qq.com>
2022-07-02 17:59:19 +0800

    全部删除

--- lib/index.tsx
+++
@@ -1,8 +0,0 @@
-import Button from "./button/button";
-import Icon from "./icon/icon";
-import Dialog, { alert, confirm, modal } from "./dialog/dialog";
-import Form from "./form/form";
-import Layout from "./layout/layout";
-import Scroll from "./scroll/scroll";
-
-export { Button, Form, Layout, Scroll, Icon, Dialog, alert, confirm, modal };
